d9d309e364138218896175561e6757cfd424ce58
Jack Contro <jack.contro.95@gmail.com>
2021-02-18 10:47:09 +0000

    cookie cutter to our project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='OCR in the context of documents',
+    author='jack contro',
+    license='MIT',
+)
